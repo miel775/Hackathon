5db4e44bd060c95a0c82b0d3395fc94f26cf7b63
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-03-24 12:32:45 +0100

    style: glow effect
    added a glow effect to the circle
    
    #5

--- hackathon/src/lib/components/black-hole.svelte
+++ hackathon/src/lib/components/black-hole.svelte
@@ -7,7 +7,6 @@
     onMount(() => {
         gsap.to(textElement, {
             rotation: 360,
-            // Use svgOrigin instead of transformOrigin for bulletproof SVG rotation
             svgOrigin: "125 125", 
             duration: 10, 
             ease: "none",
@@ -16,32 +15,36 @@
     });
 </script>
 
-<svg id="blackhole" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 250">
-  <path id="circle-path" 
-        d="M 25, 125 a 100,100 0 1,1 200,0 a 100,100 0 1,1 -200,0" 
-        fill="none" />
-        
-  <circle cx="125" cy="125" r="100" class="ring-outline" />
-  
-  <text class="orbit-text" bind:this={textElement}>
-      <textPath href="#circle-path" startOffset="5%">
-         Onderzoeksvragen
-      </textPath>
-  </text>
-</svg>
+<a href="/easteregg">
+    <svg id="blackhole" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250 250">
+    <path id="circle-path" 
+            d="M 25, 125 a 100,100 0 1,1 200,0 a 100,100 0 1,1 -200,0" 
+            fill="none" />
+            
+    <circle cx="125" cy="125" r="100" class="ring-outline" />
+    
+    <text class="orbit-text" bind:this={textElement}>
+        <textPath href="#circle-path" startOffset="5%">
+            Onderzoeksvragen
+        </textPath>
+    </text>
+    </svg>
+</a>
 
 <style>
     #blackhole {
         stroke: #fff;
         stroke-miterlimit: 10;
         font-family: "Space Mono", monospace;
-        overflow: visible;
-        /* Optional: Add this to center the SVG element itself in its container */
         display: block;
-        margin: 0 auto;
+        overflow: visible;
+        width: 256px;
+        box-shadow: 0 0 50px 30px var(--secondary-color);
+        border-radius: 50%;
     }
 
     .orbit-text {
         font-size: 32px;
     }
+
 </style>
