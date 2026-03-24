b3ca88001efb6fa2e95e095d4f919d75ad7d9433
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-03-24 10:11:32 +0100

    install: svelte

--- hackathon/src/routes/easteregg/+page.svelte
+++ hackathon/src/routes/easteregg/+page.svelte
@@ -13,7 +13,7 @@
   </head>
   <body>
 
-    <h1> Onderzoeksvragen </h1>
+    <h1> Easter Egg </h1>
 
   </body>
 </html>
--- hackathon/src/routes/onderzoeksvragen/[slug]/+page.svelte
+++ hackathon/src/routes/onderzoeksvragen/[slug]/+page.svelte
@@ -13,7 +13,7 @@
   </head>
   <body>
 
-    <h1> Onderzoeksvragen </h1>
+    <h1> Detailpagina </h1>
 
   </body>
 </html>
