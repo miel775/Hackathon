fd1760be2b04b5aef4ddc7d6a88752bbec91ef30
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2026-03-25 15:51:15 +0100

    style: content on easter egg
    on the easter egg page it should show "the-end"
    
    #8

--- src/routes/easteregg/+page.svelte
+++ src/routes/easteregg/+page.svelte
@@ -10,8 +10,39 @@
       <h1> The End </h1>
       <a href="/"> Go Back </a>
     </div>
+
+    <div id="giant-hole-wrapper">
         <Blackhole expanded={true} />
+    </div>
+
     <style>
+    h1 {
+      color: red;
+      font-size: 72px;
+    }
+
+    a {
+      width: 12em;
+      height: 3em;
+      background-color: var(--primary-color-dark);
+      color: var(--secondary-color);
+      padding: 1em;
+      border: solid 2px var(--secondary-color);
+      text-decoration: none;
+    }
+
+#giant-hole-wrapper {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: -1;
+        pointer-events: none;
+        display: flex; 
+        align-items: center;
+        justify-content: center;
+    }
+
     #easter-egg-content {
       position: relative;
       z-index: 10;
